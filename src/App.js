936580d5a29d8339170eed0342444c420bfa6f64
Jimitpatel1729 <jimitmpatel68@gmail.com>
2021-04-09 18:59:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
+import {LazyLoadImage} from 'react-lazy-load-image-component';
+
 
 function App() {
+
+  const [info,setInfo]=useState([]);
+  useEffect(()=>{
+    tryr();
+  },[]);
+ 
+  const tryr =async () =>{
+   const response=await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data=await response.json();
+    console.log(data);
+    
+    setInfo(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    {info.map(infop =>(
+      <div className="image">
+      <LazyLoadImage src={`https://avatars.dicebear.com/api/human/${infop.title}.svg`} key={infop.id}/>
+
+      </div>
+    )
+
+    )
+    }
+  </div>);
 }
 
 export default App;
